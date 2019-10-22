8f2a90608473c0177478b93b2b59d8d579859c7b
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-10-22 13:52:06 +0900

    added transition on router-view

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,30 @@
     </v-app-bar>
     <v-content>
       <router-link to="/">Top</router-link> |
-      <router-link to="/profile">Profile</router-link>
-      <router-view
-              user-name='Ryo Takazawa'
-      ></router-view>
+      <router-link to="/profile">Profile</router-link> |
+      <router-link to="/works">Works</router-link> |
+      <router-link to="/skills">Skills</router-link> |
+      <router-link to="/contact">Contact</router-link>
+      <transition
+        name="fade"
+        mode="out-in"
+      >
+        <router-view
+                user-name='Ryo Takazawa'
+        ></router-view>
+      </transition>
     </v-content>
   </v-app>
 </template>
+
+<style scoped>
+.fade-enter,
+.fade-leave-to{
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active{
+  transition: opacity 0.5s;
+}
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Top from '../components/Top.vue'
 import Profile from '../components/Profile.vue'
+import Works from '../components/Works.vue'
+import Skills from '../components/Skills.vue'
+import Contact from '../components/Contact.vue'
 
 Vue.use(VueRouter)
 
@@ -18,6 +21,30 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: Profile
+  },
+  {
+    path: '/works',
+    name: 'works',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Works
+  },
+  {
+    path: '/skills',
+    name: 'skills',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Skills
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: Contact
   }
 ]
 
