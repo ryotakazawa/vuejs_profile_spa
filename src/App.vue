f552c2aeb1f3eb3f78ce36e9935d4b672322a662
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-10-23 12:09:46 +0900

    implemented vue-scrollto #1

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,67 @@
 <template>
   <v-app>
     <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
+      <v-toolbar-title class="headline text-uppercase" @click="$vuetify.goTo(toProfile)">
         <span>ryo takazawa</span>
         
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
+        text 
+        @click="$vuetify.goTo(toProfile)">
+        <span class="mr-2">Profile</span>
+      </v-btn>
+      <v-btn
+        text 
+        @click="$vuetify.goTo(toSkills)">
+        <span class="mr-2">Skills</span>
+      </v-btn>
+      <v-btn
+        text 
+        @click="$vuetify.goTo(toWorks)">
+        <span class="mr-2">Works</span>
+      </v-btn>
+      <v-btn
+        text 
+        @click="$vuetify.goTo(toContact)">
+        <span class="mr-2">Contact</span>
       </v-btn>
     </v-app-bar>
     <v-content>
-      <router-link to="/">Top</router-link> |
-      <router-link to="/profile">Profile</router-link> |
-      <router-link to="/works">Works</router-link> |
-      <router-link to="/skills">Skills</router-link> |
-      <router-link to="/contact">Contact</router-link>
-      <transition
-        name="fade"
-        mode="out-in"
-      >
-        <router-view
-                user-name='Ryo Takazawa'
-        ></router-view>
-      </transition>
+      <Top id="top" />
+      <Profile user-name='Ryo Takazawa' style='height: 500px;' id="profile" />
+      <Skills style='height: 500px;' id="skills" />
+      <Works style='height: 500px;' id="works" />
+      <Contact style='height: 500px;' id="contact" />
     </v-content>
   </v-app>
 </template>
+
+<script>
+import Top from './components/Top';
+import Profile from './components/Profile';
+import Skills from './components/Skills';
+import Works from './components/Works';
+import Contact from './components/Contact';
+
+export default {
+  name: 'App',
+  components: {
+    Top,
+    Profile,
+    Skills,
+    Works,
+    Contact
+  },
+  data: () => ({
+    toTop: '#top',
+    toProfile: '#profile',
+    toSkills: '#skills',
+    toWorks: '#works',
+    toContact: '#contact'
+  }),
+};
+</script>
 
 <style scoped>
 .fade-enter,
--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import vuescrollto from 'vue-scrollto';
 
 Vue.config.productionTip = false
+Vue.use(vuescrollto, { offset: -60 })
 
 new Vue({
   vuetify,
   router,
+  vuescrollto,
   render: h => h(App)
 }).$mount('#app')
