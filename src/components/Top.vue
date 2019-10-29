39fe7277630f5c423f9ed836c2f4c1a4feaeb284
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-10-29 11:09:08 +0900

    modified for smart phone #16

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,26 +1,43 @@
 <template>
   <v-container>
     <v-layout
-      
+      v-if="$vuetify.breakpoint.xs"
+      text-center
+      row
+      wrap
+      >
+      <v-flex >
+        <v-img
+          :src="require('../assets/topPage.png')"
+          contain                    
+          height="450"
+        ></v-img>
+      </v-flex>
+      <v-flex>
+        <h1 class="display-3 font-weight-bold">Ryo Takazawa</h1>
+        <h4>-Could you employ me?-</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout
+      v-else
       align-center
       row
       wrap
       >
       <v-flex xs3 >
-        
       </v-flex>
-      <v-flex xs2 >
-        <h1 class="display-3 font-weight-bold pl-8">Make<br/>Better<br/>Tommorow<br/></h1>
+      <v-flex xs3 class="pl-8">
+        <h1 class="display-3 font-weight-bold">Ryo Takazawa</h1>
+        <h4>-Could you employ me?-</h4>
       </v-flex>
-      <v-flex xs6 class="pl-4">
+      <v-flex xs5 class="pl-4">
         <v-img
           :src="require('../assets/topPage.png')"
           contain                    
           height="500"
         ></v-img>
       </v-flex>
-      <v-flex xs2 >
-        
+      <v-flex xs2 > 
       </v-flex>
     </v-layout>
   </v-container>
