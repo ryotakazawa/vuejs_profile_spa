27a160c770f12006c25d6088df90cf4b2c6291dc
Ryo Takazawa <ryotakazawaengineer@gmail.com>
2019-10-30 00:11:04 +0900

    mailer

---
+++ src/plugins/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+const config = {
+  apiKey: "***REMOVED***",
+  authDomain: "ryo-takazawa.firebaseapp.com",
+  databaseURL: "ryo-takazawa.firebaseio.com",
+  projectId: "ryo-takazawa",
+  storageBucket: "ryo-takazawa.appspot.com",
+  messagingSenderId: "784862380714"
+};
+firebase.initializeApp(config);
+export const functions = firebase.functions();
